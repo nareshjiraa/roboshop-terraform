205179bee7cbb03959848da4c9740cda945e9495
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-07-01 20:19:22 +0530

    command substitution

--- servers.tf
+++ servers.tf
@@ -3,12 +3,10 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
-output "ami" {
-  value = "data.aws_ami.centos.image_id"
-}
+
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "frontend"
@@ -16,7 +14,7 @@ resource "aws_instance" "frontend" {
 }
 resource "aws_instance" "catalouge" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "catalouge"
@@ -24,7 +22,7 @@ resource "aws_instance" "catalouge" {
 }
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "cart"
@@ -32,7 +30,7 @@ resource "aws_instance" "cart" {
 }
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "mysql"
@@ -40,7 +38,7 @@ resource "aws_instance" "mysql" {
 }
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "rabbitmq"
@@ -48,7 +46,7 @@ resource "aws_instance" "rabbitmq" {
 }
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "redis"
@@ -56,7 +54,7 @@ resource "aws_instance" "redis" {
 }
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "user"
@@ -64,7 +62,7 @@ resource "aws_instance" "user" {
 }
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "shipping"
@@ -72,7 +70,7 @@ resource "aws_instance" "shipping" {
 }
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "mongodb"
@@ -80,7 +78,7 @@ resource "aws_instance" "mongodb" {
 }
 resource "aws_instance" "dispatch" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "dispatch"
