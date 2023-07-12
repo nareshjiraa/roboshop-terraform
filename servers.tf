32bb22ddbb3a57169ad8e6cd957337ae2423f300
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-07-12 10:20:41 +0530

    command substitution

--- servers.tf
+++ servers.tf
@@ -4,9 +4,12 @@ data "aws_ami" "centos" {
   owners           = ["973714476881"]
 }
 
+variable "instance_type" {
+  default = "t3.micro"
+}
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "frontend"
